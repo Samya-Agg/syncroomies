ddee182fba36302a65b0309cf3ef2c9bc9f0540d
Samya-Agg <samyaggarwal005@gmail.com>
2025-11-26 01:28:52 +0530

    Fix consumers and asgi

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
-    message = models.TextField()
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['timestamp']
+    def __str__(self):
+        return f"{self.user.username}: {self.content}"
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Message(models.Model):
+#     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+#     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+#     message = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ['timestamp']
--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,15 @@
 from django.urls import re_path
-from . import consumers
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<username>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
 ]
+
+
+
+# from django.urls import re_path
+# from . import consumers
+
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/(?P<username>\w+)/$', consumers.ChatConsumer.as_asgi()),
+# ]
